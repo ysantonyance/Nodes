513640489d52de0d0efb379e9506759c9f0b1ad8
ysantonyance <149514654+ysantonyance@users.noreply.github.com>
2025-09-27 20:19:06 +0300

    списки

---
+++ Nodes home work/Nodes home work/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+// вузол списка (або один елемент)
+class Node {
+public:
+    int value; // корисні дані (useful data)
+    Node* next; // адреса наступного вузла в списку
+
+    Node(int value) {
+        this->value = value;
+        this->next = nullptr;
+    }
+};
+
+int main()
+{
+    Node* head = nullptr;
+
+    for (int i = 0; i <= 10; i++)
+    {
+        Node* current = new Node(i);
+        current->next = head;
+        head = current;
+    }
+
+    Node* current = head;
+
+    while (current != nullptr)
+    {
+        cout << current->value << ", ";
+        current = current->next;
+    }
+    cout << "end of list\n";
+}
